1d03b4561b1a160a736ee3457507ecb0538e3399
Chrispsudo <Christer.Fagerstrand@tetrapak.com>
2024-10-13 18:46:03 +0200

    Abstrakt Player klass skapad

---
+++ Hemuppgift_Arv_Temp/Game/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    public abstract class Player
+    {
+        public string Name { get; set; }
+
+        protected Player(string name)
+        {
+            Name = name;
+        }
+
+        public abstract int TakePins(Board board);
+
+    }
+}
