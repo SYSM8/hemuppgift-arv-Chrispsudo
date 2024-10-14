e23a2c72293c5fc96474465c77a38ec7d9943206
Chrispsudo <Christer.Fagerstrand@tetrapak.com>
2024-10-14 17:14:56 +0200

    Skapat subclass för SmartComputerPlayer

---
+++ Hemuppgift_Arv_Temp/Game/SmartComputerPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    public class SmartComputerPlayer : Player
+    {
+        // Konstruktor för att ställa in userId
+
+        public SmartComputerPlayer(string userId) : base(userId) 
+        {
+            
+        }
+
+        // Implementering av TakePins för SmartComputerPlayer
+
+        public override int TakePins(Board board)
+        {
+            int numberOfPins;
+            int remainingPins = board.GetNoPins();
+        }
+
+
+    }
+}
