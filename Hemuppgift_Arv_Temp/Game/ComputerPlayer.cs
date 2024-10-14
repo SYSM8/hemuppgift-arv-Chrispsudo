e046a8220b4e977982dfe62066cdace70c5a2ffa
Chrispsudo <Christer.Fagerstrand@tetrapak.com>
2024-10-14 15:57:03 +0200

    ComputerPlayer startad.

--- Hemuppgift_Arv_Temp/Game/ComputerPlayer.cs
+++ Hemuppgift_Arv_Temp/Game/ComputerPlayer.cs
@@ -7,5 +7,32 @@ using System.Threading.Tasks;
 namespace Hemuppgift_Arv_Temp.Game
 {
     public class ComputerPlayer : Player
+    {
+
+        // Konstruktor för att ställa in userId
+
+        public ComputerPlayer(string userId) : base(userId) 
+        {
+
+        }
+
+        // Implementering av TakePins för ComputerPlayer.
+
+        public override int TakePins(Board board)
+        {
+            // Dator logik som tar ett random beslut.
+
+            Random random = new Random();
+            int numberOfPins = random.Next(1, 3);
+
+
+            // Tar stickorna från board.
+
+            Console.WriteLine($"Datorn tar {numberOfPins} stickor");
+            return board.TakePins(numberOfPins);
+
+        }
+
+    }
     
 }
